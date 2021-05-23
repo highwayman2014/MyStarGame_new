7e7e3aa5412a454cee06c750077815e14a35a3f9
highwayman2014 <shepardalex2012@gmail.com>
2021-05-23 12:07:32 +0300

    Initial commit

---
+++ android/src/com/gb/msg/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gb.msg;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gb.msg.MyStarGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyStarGame(), config);
+	}
+}
---
+++ desktop/src/com/gb/msg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gb.msg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gb.msg.MyStarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyStarGame(), config);
+	}
+}
